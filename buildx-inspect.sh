d59b461b296f0d7a1090556d75cc596045941655
Jacob Atchley <yaaqob.atchley@gmail.com>
2023-09-25 19:08:16 -0500

    fix: buildx script

--- buildx-inspect.sh
+++ buildx-inspect.sh
@@ -1,2 +1,2 @@
-docker buildx build -f ./docker-multi-test/Dockerfile -t docker-multi-test-all --platform linux/amd64 .
-docker inspect docker-multi-test-all -f "{{.Os}}/{{.Architecture}}"
+docker buildx build -f ./docker-multi-test/Dockerfile -t docker-multi-test-all --platform linux/amd64,linux/arm64,linux/arm .
+#docker inspect docker-multi-test-all -f "{{.Os}}/{{.Architecture}}"
